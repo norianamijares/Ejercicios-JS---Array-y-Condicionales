6a95fbcd528c6a83e8a5dfe68c85ccf84bfed7c5
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-27 01:33:35 -0300

    Ejercicio 11 finalizado

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,2 +1,14 @@
 //Escribe un programa que solicite al usuario una palabra y use un bucle para contar cuántas vocales tiene. Usa condicionales para verificar si cada letra es una vocal.
 
+let palabra = prompt("Introduce una palabra: ");
+let contadorVocales = 0;
+
+for (let i = 0; i < palabra.length; i++) {
+    let letra = palabra[i].toLowerCase();
+
+    if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+        contadorVocales++;
+    }
+}
+
+console.log("La palabra '" + palabra + "' tiene " + contadorVocales + " vocales.");
